356f8998ebc469b5301ea8d8ff5e25d1b6b5df77
kinam-Lee <shine1700@gmail.com>
2020-02-10 11:25:29 +0900

    -kinam-v.2.6-final

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor"); 
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-
-const INITIAL_COLOR = "060606" ; 
+const INITIAL_COLOR = "#000000" ;
+const INITIAL_BG_COLOR  = "#ffffff"; 
 const CANVAS_SIZE = 700; 
 
 canvas.width = CANVAS_SIZE; 
 canvas.height = CANVAS_SIZE ; 
 
 ctx.strokeStyle = INITIAL_COLOR; 
-ctx.lineWidth = 2.5; 
-
+ctx.lineWidth = 2.5;
+/* 이미지 저장시 default 값이 투명이므로 이에대한 처리를 위한 초기화작업 */ 
+ctx.fillStyle = INITIAL_BG_COLOR; 
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 /*
 // fillStyle : 캔버스의 배경색을 바꾸는것, strokeStyle : 도형이나 line의 색을 변경하는것
@@ -92,12 +95,29 @@ function handleCanvasClick(){
     }
 }
 
+
+function handleContextMenu(event){
+    //console.log(event);
+    event.preventDefault(); /* 이걸 씀으로서 인해 event.defaultPrevented 값이 true로 변경 - 즉 우클릭 금지*/
+}
+
+function handelSaveBtnClick(){
+    const imageDataURL = canvas.toDataURL(); // defualut 저장포맷은 png
+    //console.log(imageDataURL);
+    const link = document.createElement("a");
+    link.href = imageDataURL
+    link.download = "JSPaint"; 
+    //console.log(link);
+    link.click();
+}   
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove); 
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleContextMenu);
 }
 
 //console.log(Array.from(colors));
@@ -113,4 +133,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handelSaveBtnClick);
 }
